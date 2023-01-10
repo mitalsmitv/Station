266b77c778701d48452b8a921742a93023d7b6f3
mitalpatel5944 <43605009+mitalpatel5944@users.noreply.github.com>
2023-01-10 17:11:16 +0530

    INITIAL

---
+++ App.js
@@ -0,0 +1,22 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/screen/Login';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
