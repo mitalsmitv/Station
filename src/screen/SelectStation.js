4ebfdb743f2428cf0c32410a8a8d5fb9fc731eb3
mitalpatel5944 <43605009+mitalpatel5944@users.noreply.github.com>
2023-01-12 19:45:28 +0530

    api

--- src/screen/SelectStation.js
+++ src/screen/SelectStation.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, ImageBackground, Image, TouchableOpacity, StyleSheet, TextInput, FlatList } from "react-native";
 
 function SelectStation(props) {
+    const[data,setdata]=useState([])
+    useEffect(()=>{
+getStations()
+    },[])
+    function getStations() {
+        fetch("https://reqres.in/api/unknown",
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+        .then(response=>response.json())
+        .then(response=>{
+            console.log(response)
+            setdata(response.data)
+        })
+    }
     return (
         <View style={{
             flex: 1
@@ -16,16 +36,17 @@ function SelectStation(props) {
                         Search by ID, Name, City
                     </TextInput></View>
                 <FlatList
-                    data={[1, 2, 3, 4, 5]}
+                    data={data}
                     renderItem={({ item, index }) => {
+                        console.log(item)
                         return (
                             <TouchableOpacity style={styles.viewf}
-                            onPress={() => {
-                                props.navigation.navigate('Details')
-                              }}
+                                onPress={() => {
+                                    props.navigation.navigate('Details')
+                                }}
                             >
                                 <Image source={require('../Images/png/Pump.png')} />
-<View><Text style={styles.textn}>1041700</Text><Text style={styles.textt}>ISLAND_LAKE_BP</Text></View>
+                                <View><Text style={[styles.textn,{color:item.color}]}>{item?.pantone_value}</Text><Text style={styles.textt}>{item.name}</Text></View>
                             </TouchableOpacity>
                         )
                     }}
@@ -47,15 +68,15 @@ const styles = StyleSheet.create({
         color: "#000000",
         alignSelf: "center",
         marginTop: 24,
-        fontFamily:"Poppins-Bold"
+        fontFamily: "Poppins-Bold"
     },
     textinput: {
         color: "#ADB7C6",
         fontSize: 14,
         marginLeft: 10,
         width: "80%",
-        fontFamily:"Poppins-Medium",
-        marginTop:5
+        fontFamily: "Poppins-Medium",
+        marginTop: 5
     },
     views: {
         flexDirection: "row",
@@ -67,8 +88,8 @@ const styles = StyleSheet.create({
         alignSelf: "center",
         backgroundColor: "#f0f4f5",
         borderRadius: 12,
-        marginBottom:15,
-        alignItems:"center"
+        marginBottom: 15,
+        alignItems: "center"
     },
     viewm: {
         borderWidth: .5,
@@ -76,28 +97,28 @@ const styles = StyleSheet.create({
         borderRadius: 30,
         marginTop: 3,
         borderColor: "#ffffff",
-        flex:1
+        flex: 1
     },
-    textn:{
-        fontSize:16,
-        fontFamily:"Poppins-Bold",
-        color:"#000000",
-        marginLeft:30
+    textn: {
+        fontSize: 16,
+        fontFamily: "Poppins-Bold",
+        color: "#000000",
+        marginLeft: 30
     },
-    textt:{
-        fontSize:13,
-        color:"#ADB7C6",
-        marginLeft:30,
-        marginBottom:10,
-        fontFamily:"Poppins-Medium"
+    textt: {
+        fontSize: 13,
+        color: "#ADB7C6",
+        marginLeft: 30,
+        marginBottom: 10,
+        fontFamily: "Poppins-Medium"
     },
-    viewf:{
-        borderBottomWidth:1,
-        borderColor:"#f0f4f5",
-        flexDirection:"row",
-    marginRight:30,
-    marginLeft:30,
-    marginTop:10,
-    marginBottom:15
+    viewf: {
+        borderBottomWidth: 1,
+        borderColor: "#f0f4f5",
+        flexDirection: "row",
+        marginRight: 30,
+        marginLeft: 30,
+        marginTop: 10,
+        marginBottom: 15
     }
 })
